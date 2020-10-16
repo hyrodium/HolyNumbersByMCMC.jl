3de6f0f63b55f6f1681d9ae73ab2432eca41b808
hyrodium <hyrodium@gmail.com>
2020-10-17 02:15:47 +0900

    faster impl

--- linkage.jl
+++ linkage.jl
@@ -1,13 +1,44 @@
 import Luxor
-using LinearAlgebra
 using Colors
 using BenchmarkTools
 
-function cci(a,b,r,s)
+struct Point
+    x::Float64
+    y::Float64
+end
+
+struct Matrix
+    xx::Float64
+    yx::Float64
+    xy::Float64
+    yy::Float64
+end
+
+function norm(a::Point)
+    return sqrt(a.x^2+a.y^2)
+end
+
+function Base.:+(a::Point,b::Point)
+    return Point(a.x+b.x, a.y+b.y)
+end
+
+function Base.:-(a::Point,b::Point)
+    return Point(a.x-b.x, a.y-b.y)
+end
+
+function Base.:/(a::Point,b::Real)
+    return Point(a.x/b, a.y/b)
+end
+
+function Base.:*(M::Matrix, a::Point)
+    return Point(M.xx*a.x+M.xy*a.y, M.yx*a.x+M.yy*a.y)
+end
+
+function cci(a::Point,b::Point,r,s)
     L=norm(b-a)
     m=(r+s)/L
     n=(r-s)/L
-    M=[m*n -sqrt((m^2-1)*(1-n^2));sqrt((m^2-1)*(1-n^2)) m*n]
+    M=Matrix(m*n, sqrt((m^2-1)*(1-n^2)), -sqrt((m^2-1)*(1-n^2)), m*n)
     return (a+b)/2 + M*(b-a)/2
 end
 
@@ -27,19 +58,19 @@ struct HolyNumbers
     w13::Float64
 end
 
-struct PointPositions
-    q1::Array{Float64,1}
-    q2::Array{Float64,1}
-    q3::Array{Float64,1}
-    q4::Array{Float64,1}
-    q5::Array{Float64,1}
-    q6::Array{Float64,1}
-    q7::Array{Float64,1}
-    q8::Array{Float64,1}
-    function PointPositions(t,holynumbers::HolyNumbers)
-        q1 = [0.0,0.0]
-        q2 = q1 + w13*[cos(t),sin(t)]
-        q3 = [holynumbers.w01,-holynumbers.w12]
+struct Points
+    q1::Point
+    q2::Point
+    q3::Point
+    q4::Point
+    q5::Point
+    q6::Point
+    q7::Point
+    q8::Point
+    function Points(t,holynumbers::HolyNumbers)
+        q1 = Point(0.0,0.0)
+        q2 = q1 + Point(w13*cos(t),w13*sin(t))
+        q3 = Point(holynumbers.w01,-holynumbers.w12)
         q4 = cci(q2,q3,holynumbers.w10,holynumbers.w02)
         q5 = cci(q3,q2,holynumbers.w03,holynumbers.w11)
         q6 = cci(q4,q3,holynumbers.w05,holynumbers.w04)
@@ -49,8 +80,8 @@ struct PointPositions
     end
 end
 
-function lxrpt(x;unitlength = 5)
-    Luxor.Point(unitlength*x[1],-unitlength*x[2])
+function lxrpt(q;unitlength = 5)
+    Luxor.Point(unitlength*q.x,-unitlength*q.y)
 end
 
 
@@ -74,11 +105,11 @@ holynumbers = HolyNumbers(w01,w02,w03,w04,w05,w06,w07,w08,w09,w10,w11,w12,w13)
 
 
 n=90
-@benchmark ptss = [PointPositions(2π*i/n,holynumbers) for i in 0:n-1]
+ptss = [Points(2π*i/n,holynumbers) for i in 0:n-1]
 locus=[ptss[i].q8 for i in 1:n]
 
 
-function draw(filename, pts::PointPositions, locus)
+function draw(filename, pts::Points, locus)
     q1 = pts.q1
     q2 = pts.q2
     q3 = pts.q3
